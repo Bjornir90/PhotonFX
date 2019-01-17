124c9ff6f95b6b42f581b51e91c96034afd29887
Bjornir90 <celestincollin@gmail.com>
2019-01-17 10:29:46 +0100

    Initial commit, with basic Particle behavior and state and simple ParticleEnvironment.

---
+++ src/engine/particle/Particle.java
@@ -0,0 +1,46 @@
+package engine.particle;
+
+import org.newdawn.slick.Color;
+import org.newdawn.slick.Graphics;
+
+import static engine.particle.ParticleEnvironment.gravityX;
+import static engine.particle.ParticleEnvironment.gravityY;
+
+public class Particle {
+    private float weight;
+    private float brigthness;
+    private float drag;
+    private float size;
+    private Color color, lightColor;
+    private float posX;
+    private float posY;
+    private float speedX;
+    private float speedY;
+
+    protected Particle(float weight, float brigthness, float drag, Color color, Color lightColor, float posX, float posY, float speedX, float speedY, float size) {
+        this.weight = weight;
+        this.brigthness = brigthness;
+        this.drag = drag;
+        this.color = color;
+        this.lightColor = lightColor;
+        this.posX = posX;
+        this.posY = posY;
+        this.speedX = speedX;
+        this.speedY = speedY;
+        this.size = size;
+    }
+
+    protected void render(Graphics g){
+        Color oldColor = g.getColor();
+        g.setColor(color);
+        g.drawRect(posX, posY, size, size);
+        g.setColor(oldColor);
+    }
+
+    protected void update(int delta){
+        speedX += drag*ParticleEnvironment.windX + weight*gravityX;
+        speedY += drag*ParticleEnvironment.windY + weight*gravityY;
+        posX += delta*speedX;
+        posY += delta*speedY;
+    }
+}
---
+++ src/engine/particle/ParticleEnvironment.java
@@ -0,0 +1,14 @@
+package engine.particle;
+
+public class ParticleEnvironment {
+    public static float windX, windY, gravityX, gravityY;
+
+    //Default values
+    static{
+        windX = 0.0f;
+        windY = 0.0f;
+        gravityX = 0.0f;
+        gravityY = 1.0f;
+    }
+
+}
